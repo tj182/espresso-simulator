324b77da891f69191806f8d83d27b6bf965c9e64
Oleg Zheleznyi <tj182@yandex.ru>
2019-05-30 14:40:14 +0300

    added simple main activity

---
+++ app/src/main/java/com/ferrup/espresser/ui/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ferrup.espresser.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ferrup.espresser.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
