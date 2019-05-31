fbfaf007cfed85beb729667d0d3a339d99db04b5
Oleg Zheleznyi <tj182@yandex.ru>
2019-05-31 14:34:36 +0300

    added butterknife

--- app/src/main/java/com/ferrup/espresser/ui/main/MainActivity.java
+++ app/src/main/java/com/ferrup/espresser/ui/main/MainActivity.java
@@ -2,9 +2,14 @@ package com.ferrup.espresser.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.ferrup.espresser.R;
+import com.ferrup.espresser.ui.settings.SettingsActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.settings_button)
+    public void onSettingsClicked() {
+        startActivity(new Intent(this, SettingsActivity.class));
     }
 }
