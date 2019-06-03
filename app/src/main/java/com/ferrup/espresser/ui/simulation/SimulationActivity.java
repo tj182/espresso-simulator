824b0e164628eee140e0d2c98afcf7e57e41eb90
Oleg Zheleznyi <tj182@yandex.ru>
2019-06-03 04:28:55 +0300

    clean TODO

--- app/src/main/java/com/ferrup/espresser/ui/simulation/SimulationActivity.java
+++ app/src/main/java/com/ferrup/espresser/ui/simulation/SimulationActivity.java
@@ -108,9 +108,6 @@ public class SimulationActivity extends AppCompatActivity {
         simulator.setOnStateChangeListener(this::onSimulatorStateChange);
         onDataChange(simulator.getData());
         simulator.setOnDataChangeListener(this::onDataChange);
-
-        // TODO: setup views with data
-
     }
 
     private void initProgressBars() {
